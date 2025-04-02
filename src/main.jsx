f51d20875e20e7442e5172b65c009e5e951d8f22
Iago Cunha <iagocunha51@gmail.com>
2025-04-02 17:42:22 +0000

    Atualização do main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <img class="logo" src="https://www.observatorio.ind.br/wp-content/themes/fiec/images/logo.svg" alt="logo observatorio da industria"></img>
     <App />
   </StrictMode>,
 )
